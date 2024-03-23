11b2e93eea4784e46ef1fc2b2c2785e2523f520e
taipham <taipham56ld3@gmail.com>
2024-03-24 00:50:27 +0700

    [DOC] E1: Add docstring for greatest common divisor of string

--- exercise/greatest_common_divisor.py
+++ exercise/greatest_common_divisor.py
@@ -1,5 +1,27 @@
 
 def gcdOfStrings(str1: str, str2: str) -> str:
+    """
+    Returns the greatest common divisor of two strings.
+
+    Given two strings `str1` and `str2`, this function computes the greatest common divisor
+    (GCD) of their lengths and returns the substring that divides both `str1` and `str2`.
+
+    Args:
+        str1 (str): The first input string.
+        str2 (str): The second input string.
+
+    Returns:
+        str: The greatest common divisor substring of `str1` and `str2`, or an empty string if
+        no such substring exists.
+
+    Example:
+        >>> gcdOfStrings("ABCABC", "ABC")\n
+        >>> result: 'ABC'\n
+        >>> gcdOfStrings("ABABAB", "ABAB")\n
+        >>> result: 'AB'\n
+        >>> gcdOfStrings("LEET", "CODE")\n
+        >>> result: ''
+    """
     def gcd(a, b):
         while b:
             a, b = b, a % b
